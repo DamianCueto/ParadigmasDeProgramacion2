a2097e0e9d7346558e56cb93be2cf193894c7d43
DamianCueto <124367981+DamianCueto@users.noreply.github.com>
2023-02-22 08:17:26 -0600

    Add files via upload
    cat

--- App.java
+++ App.java
@@ -1,41 +1,25 @@
 package com.pp2;
 
-class Runner1 implements Runnable {
-
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		for (int i=0; i<10; i++)
-			System.out.println("Runner 1: " + i);
-	}
-	
-}
-
-
-class Runner2 implements Runnable {
-
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		for (int i=0; i<10; i++)
-			System.out.println("Runner 2: " + i);
-	}
-
-}
-
-
 public class App {
 	
-	public static void main (String [] args) {
+	public static void drawLegs(){
+        System.out.println("     ||     ||      ");
+        System.out.println("     ||     ||      ");
+        System.out.println("    (||)   (||)     ");
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		
-		Thread t1 = new Thread (new Runner1());
-		Thread t2 = new Thread (new Runner2());
-		t1.start();
-		t2.start();
+		System.out.println("   /\\         /\\   ");
+        System.out.println("  /  \\_______/  \\  ");
+        System.out.println(" /               \\ ");
+        System.out.println("(  /\\         /\\  )");
+        System.out.println("====     V     ====");
+        System.out.println("======(__|__)======");
+        System.out.println("  (             )  ");
+        System.out.println("   (___________)   ");
+        drawLegs();
 	}
-	
-	
-	
-	
+
 }
-		
