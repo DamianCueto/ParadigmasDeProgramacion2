594836895174670ffa76be13ac263a623d5983f6
DamianCueto <124367981+DamianCueto@users.noreply.github.com>
2023-05-31 08:14:52 -0600

    Add files via upload

---
+++ Leccion2_2/src/com/pp2/App.java
@@ -0,0 +1,21 @@
+package com.pp2;
+
+public class App { 
+	
+	public static void drawLegs(){
+    System.out.println("     ||     ||      ");
+    System.out.println("     ||     ||      ");
+    System.out.println("    (||)   (||)     ");
+}
+	public static void main(String[] args) {
+        System.out.println("   /\\         /\\   ");
+        System.out.println("  /  \\_______/  \\  ");
+        System.out.println(" /               \\ ");
+        System.out.println("(  /\\         /\\  )");
+        System.out.println("====     V     ====");
+        System.out.println("======(__|__)======");
+        System.out.println("  (             )  ");
+        System.out.println("   (___________)   ");
+        drawLegs();
+    }    
+}
